83b17a30739c423a7364cff90b0d2ce12bbfc035
claudgage <claudio@trend.io>
2023-04-29 14:29:36 -0600

    tested and created functions to parse the JSON mock data for both client and policy. Next: work on logic to find/filter the correct id for policies and clients; might separate logic into different files but for now I will keep it in a single file, easier for testing

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { Client, Policy } from './interfaces';
-
+import clientsJson from '../clientsData.json';
+import policiesJson from '../policyData.json';
 @Injectable()
 export class AppService {
   getHello(): string {
+    obtainClientJson(clientsJson);
     return 'Hello World!';
   }
+  getClient(): any {
+    obtainClientJson(clientsJson);
+    return 'something';
+  }
+  getPolicy(): any {
+    obtainPolicyJson(policiesJson);
+    return 'something';
+  }
+  linkPolicyAndClient(): any {
+    return 'something';
+  }
 }
-async function obtainClientJson(clientJson: JSON) {
+function obtainClientJson(clientJson): Client[] {
   const clients: JSON = clientJson;
+  const parsedClients = JSON.parse(JSON.stringify(clients)).clients;
+  return parsedClients;
 }
 
-async function obtainPolicyJson(policyJson: JSON) {
-  const clients: JSON = policyJson;
+function obtainPolicyJson(policyJson): Policy[] {
+  const policies: JSON = policyJson;
+  const parsedPolicies = JSON.parse(JSON.stringify(policies)).policies;
+  return parsedPolicies;
 }
